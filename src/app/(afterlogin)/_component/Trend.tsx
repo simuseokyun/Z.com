05bf49aeadc4969dd535c3c9e05a5d990af98635
simuseokyun <sim31059999@gmail.com>
2025-02-04 23:04:12 +0900

    refactor : 간단한 수정

--- src/app/(afterlogin)/_component/Trend.tsx
+++ src/app/(afterlogin)/_component/Trend.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
 import style from './trend.module.css';
+import { Hashtag } from '@/model/Hashtag';
 
-export default function Trend({ trend }: { trend: { title: string; count: number } }) {
+type Prop = { trend: Hashtag };
+export default function Trend({ trend }: Prop) {
     return (
         <Link href={`/search?q=${encodeURIComponent(trend.title)}`} className={style.container}>
-            {/* 주소창에 #이 들어가면 그 뒤로는 서버에 전송이 안된다. 이럴 때 encodeURIComponent라는 함수를 사용
-            # => %23 으로 바꿔주는 함수 */}
+            <div className={style.count}>실시간트렌드</div>
             <div className={style.title}>{trend.title}</div>
-            <div className={style.count}>{trend.title}</div>
-            <div className={style.count}>{trend.count} posts</div>
+            <div className={style.count}>{trend.count.toLocaleString()} posts</div>
         </Link>
     );
 }
