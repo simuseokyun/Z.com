4c85cb8e2d2f1850c4e967d1b329173a917d324d
simuseokyun <sim31059999@gmail.com>
2025-02-04 22:35:46 +0900

    feat : 게시하기 svg 추가

--- src/app/(afterlogin)/layout.tsx
+++ src/app/(afterlogin)/layout.tsx
@@ -6,7 +6,7 @@ import ZLogo from '../../../public/5Udwvqim.jpg';
 import NavMenu from './_component/NavMenu';
 import TrendSection from './_component/TrendSection';
 import LogoutButton from './_component/LogoutButton';
-import FollowRecommend from './_component/FollowRecommend';
+
 import RightSearchZone from './_component/RightSearchZone';
 import { auth } from '@/auth';
 import RQProvider from './_component/RQProvider';
@@ -37,9 +37,18 @@ export default async function AfterLoginLayout({ children, modal }: Props) {
                             </ul>
                             <Link href="/compose/tweet" className={style.postButton}>
                                 <span>게시하기</span>
+                                <svg
+                                    viewBox="0 0 24 24"
+                                    aria-hidden="true"
+                                    className="r-jwli3a r-4qtqp9 r-yyyyoo r-1472mwg r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp"
+                                >
+                                    <g>
+                                        <path d="M23 3c-6.62-.1-10.38 2.421-13.05 6.03C7.29 12.61 6 17.331 6 22h2c0-1.007.07-2.012.19-3H12c4.1 0 7.48-3.082 7.94-7.054C22.79 10.147 23.17 6.359 23 3zm-7 8h-1.5v2H16c.63-.016 1.2-.08 1.72-.188C16.95 15.24 14.68 17 12 17H8.55c.57-2.512 1.57-4.851 3-6.78 2.16-2.912 5.29-4.911 9.45-5.187C20.95 8.079 19.9 11 16 11zM4 9V6H1V4h3V1h2v3h3v2H6v3H4z"></path>
+                                    </g>
+                                </svg>
                             </Link>
                             <LogoutButton me={session} />
-                            {/* LogoutButton에서 그냥 useSession()을 사용해서 정보를 받아오면 될 줄 알았는데 그렇게 하면 로그아웃 버튼이 나타나질 않음 => 그래서 prop으로 전달해줫음 */}
+                            {/* // 회원가입 후 logout에서 useSession값이 바로 업데이트 되지않음. 따라서 props로 전달 */}
                         </nav>
                     </div>
                 </section>
