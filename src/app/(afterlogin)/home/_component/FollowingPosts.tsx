a88c4dc290f974ceeefc25fd07523feddae3e612
simuseokyun <sim31059999@gmail.com>
2025-02-04 22:37:30 +0900

    refactor : 팔로잉 포스트 infiniteQuery 변경

--- src/app/(afterlogin)/home/_component/FollowingPosts.tsx
+++ src/app/(afterlogin)/home/_component/FollowingPosts.tsx
@@ -1,19 +1,46 @@
 'use client';
 
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { InfiniteData, useInfiniteQuery, useSuspenseInfiniteQuery, useSuspenseQuery } from '@tanstack/react-query';
 import { getFollowingPosts } from '../_lib/getFollowingPosts';
 import Post from '@/app/(afterLogin)/_component/Post';
 import { Post as IPost } from '@/model/Post';
-
+import { Fragment, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 export default function FollowingPosts() {
-    const { data, isPending } = useSuspenseQuery<IPost[]>({
+    const { ref, inView } = useInView({
+        threshold: 0, // 얼마정도 드러나야 실행할건지 0 ~ 1 사이값
+        delay: 0,
+    });
+    const { data, isFetching, isError, hasNextPage, fetchNextPage } = useSuspenseInfiniteQuery<
+        IPost[],
+        Object,
+        InfiniteData<IPost[]>,
+        [_1: string, _2: string],
+        number
+    >({
         queryKey: ['posts', 'followings'],
         queryFn: getFollowingPosts,
-        staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
+        initialPageParam: 0,
+        getNextPageParam: (lastPage) => lastPage.at(-1)?.postId,
+        staleTime: 60 * 1000,
         gcTime: 300 * 1000,
     });
-    console.log(data);
 
-    console.log(data);
-    return data?.map((post) => <Post key={post.postId} post={post} />);
+    useEffect(() => {
+        if (inView) {
+            !isFetching && hasNextPage && fetchNextPage();
+        }
+    }, [inView, inView, isFetching, hasNextPage]);
+    return (
+        <>
+            {data?.pages.map((page, i) => (
+                <Fragment key={i}>
+                    {page.map((post, z) => (
+                        <Post key={z} post={post} />
+                    ))}
+                </Fragment>
+            ))}
+            <div ref={ref} style={{ height: 50 }} />
+        </>
+    );
 }
--- src/app/(afterlogin)/home/_lib/getFollowingPosts.ts
+++ src/app/(afterlogin)/home/_lib/getFollowingPosts.ts
@@ -1,5 +1,7 @@
-export async function getFollowingPosts() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/followings`, {
+type Props = { pageParam?: number };
+export async function getFollowingPosts({ pageParam }: Props) {
+    // pageParam은 React Query에서 useInfiniteQuery를 사용할 때 페이지네이션을 위한 getNextPageParam과 함께 사용되는 파라미터입니다.
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/followings?cursor=${pageParam}`, {
         next: {
             tags: ['posts', 'followings'],
         },
