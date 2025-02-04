56463c615c603ec61e52b7f7f4fa1bbe31cde90e
simuseokyun <sim31059999@gmail.com>
2025-02-04 22:37:00 +0900

    feat : 임시 주석

--- src/app/(afterlogin)/home/_component/TabDeciderSuspense.tsx
+++ src/app/(afterlogin)/home/_component/TabDeciderSuspense.tsx
@@ -8,6 +8,7 @@ export default async function TabDeciderSuspense() {
         queryKey: ['posts', 'recommends'],
         queryFn: getPostRecommends,
         initialPageParam: 0, // 처음 커서 값
+        // prefetchQuery는 특정 페이지만 미리 요청하고 다음 페이지를 가져올 필요가 없기때문에 getNextPageParam 속성이 필수적으로 필요하지 않다.
         staleTime: 30 * 1000, // 이 쿼리의 데이터는 30초 동안 신선함
         gcTime: 40 * 1000,
     });
