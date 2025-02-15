563dc62f3f6c7189eabe05bfa19f8187cfcf5a6d
simuseokyun <sim31059999@gmail.com>
2025-02-15 14:50:46 +0900

    refactor : fetch 함수 수정

--- src/app/(afterlogin)/messages/_lib/getMessages.ts
+++ src/app/(afterlogin)/messages/_lib/getMessages.ts
@@ -1,27 +1,25 @@
-import { cookies } from 'next/headers'
 import { Message } from '@/model/Message'
 
-interface Props {
-  pageParam: number
+type Props = {
+  pageParam?: number
   queryKey: [string, { senderId: string; receiverId: string }, string]
 }
-
-const getMessages = async ({ pageParam, queryKey }: Props) => {
+export default async function getMessages({ pageParam, queryKey }: Props) {
   const [one, userInfo] = queryKey
-  const response = await fetch(
+  const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userInfo.senderId}/rooms/${userInfo.receiverId}?cursor=${pageParam}`,
     {
-      next: { tags: ['rooms'] },
+      next: {
+        tags: ['rooms'],
+      },
       credentials: 'include',
-      // headers: { Cookie: (await cookies()).toString() }, // 서버컴포넌트에서 데이터 가져올 땐 Cookie 값 넣어줄 것
-      cache: 'no-cache',
+      cache: 'no-store',
     },
   )
 
-  if (!response.ok) {
-    throw new Error('에러')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
   }
-  return response.json() as Promise<Message[]>
-}
 
-export default getMessages
+  return res.json() as Promise<Message[]>
+}
