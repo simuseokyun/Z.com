8e01f41fc6965076e494b91106bc12c6622f10bd
simuseokyun <sim31059999@gmail.com>
2025-02-15 14:51:35 +0900

    feat : 채팅 목록 없을때 메시지

--- src/app/(afterlogin)/messages/page.tsx
+++ src/app/(afterlogin)/messages/page.tsx
@@ -6,16 +6,19 @@ import Room from './_component/Room'
 export default async function Home() {
   const session = await auth()
   const rooms = session?.user?.email ? await getRooms(session?.user?.email) : []
-  console.log(rooms)
 
   return (
     <main className={style.main}>
       <div className={style.header}>
         <h3>쪽지</h3>
       </div>
-      {rooms.map((room) => (
-        <Room key={room.room} room={room} />
-      ))}
+      {rooms.length ? (
+        rooms.map((room) => <Room key={room.room} room={room} />)
+      ) : (
+        <p style={{ textAlign: 'center', fontWeight: 500 }}>
+          메시지 목록이 존재하지 않습니다.
+        </p>
+      )}
     </main>
   )
 }
